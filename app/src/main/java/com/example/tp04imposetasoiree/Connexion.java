aff3fe4bdabd6c00ef447da4eb89d9e890a896bb
akecebasbenarif <kecebasbenarif12728@lasalle63.fr>
2022-11-25 11:44:52 +0100

    Page Inscription

--- app/src/main/java/com/example/tp04imposetasoiree/Connexion.java
+++ app/src/main/java/com/example/tp04imposetasoiree/Connexion.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,6 +14,7 @@ import org.json.JSONObject;
 
 public class Connexion extends AppCompatActivity {
     private Button buttonIdentifier;
+    private Button buttonInscription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +29,12 @@ public class Connexion extends AppCompatActivity {
 
             createAndExecuteConnexionMembres(login, mdp);
         });
+        buttonInscription = (Button) findViewById(R.id.buttonInscription);
+        buttonInscription.setOnClickListener(view -> {
+            Intent i = new Intent(this, Inscription.class);
+            startActivity(i);
+        });
+
 
     }
 
@@ -51,7 +57,7 @@ public class Connexion extends AppCompatActivity {
         try {
             JSONObject jsonObject = new JSONObject(s);
             if (jsonObject.getBoolean("success")) {
-                Intent i = new Intent(this, Inscription.class);
+                Intent i = new Intent(this, SoireeAVenir.class);
                 startActivity(i);
             } else {
                 Toast.makeText(this, "Mauvais login ou mdp", Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/tp04imposetasoiree/Inscription.java
+++ app/src/main/java/com/example/tp04imposetasoiree/Inscription.java
@@ -2,13 +2,64 @@ package com.example.tp04imposetasoiree;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Inscription extends AppCompatActivity {
+    private Button buttonValider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main3);
+        buttonValider = (Button) findViewById(R.id.buttonValider);
+        buttonValider.setOnClickListener(view -> {
+            String login = ((EditText) (findViewById(R.id.etLog2))).getText().toString();
+            String nom = ((EditText) (findViewById(R.id.etNom))).getText().toString();
+            String mdp = ((EditText) (findViewById(R.id.etMdp1))).getText().toString();
+            String prenom = ((EditText) (findViewById(R.id.etPrenom))).getText().toString();
+            String ddn = ((EditText) (findViewById(R.id.etDdn))).getText().toString();
+            String mail = ((EditText) (findViewById(R.id.etMail))).getText().toString();
+            createAndExecuteInscriptionMembres(login, mdp, nom, prenom, ddn, mail);
+        });
+
+
+    }
+
+    private void createAndExecuteInscriptionMembres(String login, String mdp, String nom, String prenom, String ddn, String mail) {
+        WSConnexionHTTPS ws = new WSConnexionHTTPS() {
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                traiterRetourGetInscriptionMembres(s);
+            }
+
+        };
+        ws.execute("requete=creerCompte&login=" + login + "&nom=" + nom + "&prenom=" + prenom + "&ddn=" + ddn + "&mail=" + mail + "&password=" + mdp);
+
+    }
+
+    private void traiterRetourGetInscriptionMembres(String s) {
+        Log.d("TRAITER-RETOUR-INSCRIPTION-MEMBRES", s);
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+            if (jsonObject.getBoolean("success")) {
+                Intent i = new Intent(this, MainActivity4.class);
+                startActivity(i);
+                Toast.makeText(this, "Inscription validée", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Erreur", Toast.LENGTH_SHORT).show();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 }
